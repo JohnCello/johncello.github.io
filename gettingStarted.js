2a8a66ce7b7ab708ec6821fc623dfe451586a0c8
JMReed <JMReed@hvc.rr.com>
2018-09-17 10:25:53 -0400

    initial revision

---
+++ gettingStarted.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+
+function nextMethod (x) {
+	var res = "page" + x + ".html";
+	window.location.href = res;
+}
+
+
+function setupCompleted (key, value) {
+//	var dict = [];
+	var key = [];
+	var value = [];
+	
+	key.push(key);
+	value.push(value);
+
+//	dict.push({
+ //	   key: "key",
+  	//  value: "value"
+//	});
+	alert(key, value);
+}
+
+
